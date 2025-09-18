f4e8471c7cd1b3fb31529e21daf9b83fbe6453d2
Eniola Omotee <40490137+eniolaomotee@users.noreply.github.com>
2025-09-18 16:18:12 +0100

    dockerize python

---
+++ Dockerfile.py
@@ -0,0 +1,37 @@
+# Build from a slim Debian(image) image
+FROM debian:stable-slim
+
+# Update apt
+RUN apt update
+RUN apt upgrade -y
+
+# Install build tooling
+RUN apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+
+# Download python interpreter code and unpack it
+RUN wget https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tgz
+RUN tar -xf Python-3.10.*.tgz
+
+# Build the python interpreter from source
+RUN cd Python-3.10.8 && ./configure --enable-optimizations && make && make altinstall
+
+# Copy our code into the image 
+COPY main.py main.py
+
+# Copy our data dependency into the image
+COPY books/ books/
+
+CMD ["python3.10", "main.py"]
+
+
+
+# A simpler version using the official python image
+
+# FROM python:3.12-slim
+
+# WORKDIR /app
+
+# COPY main.py main.py
+# COPY books/ books/
+
+# CMD ["python", "main.py"]
